0817290dd7a7c28e98895f4e4fada3f0cd8777cc
toukoubun <tanghaowen@team-lab.com>
2020-02-21 01:51:59 +0900

    いらない関数を削除

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -8,19 +8,7 @@ use Illuminate\Support\Facades\Storage;
 class Material extends DaisyModelBase
 {
     protected $table = 'materials';
-
-    // クラウドストレージからコンテンツ取得
-    public function getObject()
-    {
-        $s3_disk = Storage::disk('s3');
-        $mimeType = $s3_disk->mimeType('materials/' . $this->name);
-        // $config = self::di()->get('config');
-        // $result = $this->storageHelper->getObject($config->s3->files->bucket, $config->s3->files->prefix . '/' . $this->name);
-
-        // return $result;
-        return $mimeType;
-    }
-
+    
     public static function getItemByName($material_name): ?Material
     {
         return Material::where('name', $material_name)->first();
