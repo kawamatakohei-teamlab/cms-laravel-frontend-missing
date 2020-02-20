0969caaca0fdb5c9c2817685132cd584915390d1
tanghaowen <tanghaowen@team-lab.com>
2020-02-20 16:57:44 +0900

    test用のControllerを追加、AWSとMino対応

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Stylesheet;
+use App\javascript;
+use Illuminate\Support\Facades\Storage;
+
+class TestController extends Controller
+{
+    public function index(Request $request)
+    {
+        $s3_disk = Storage::disk('s3');
+        $file = $s3_disk->get("materials/company-info_sp@2x.png");
+        return "Hello";
+    }
+
+}
