7e893c3ad005437ccaf5dc85bc3cc684a3329592
keitaogawa-teamlab <in-ogawakeita@team-lab.com>
2020-02-25 17:55:49 +0900

    関数作成

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -17,9 +17,16 @@ class Category extends DaisyModelBase
 
     public static function getAllCategories(): ?Category
     {
-        $all_categories_items = Category::all();
+        $parent_categories_items = Category::where('parent',0)->get();
+        $all_categories_items = [];
+        foreach ($parent_categories_items as $parent_category) {
+            $all_categories_items[$parent_category["slug"]]["display_name"] = $parent_category["name"];
+            $child_categories = Category::where('parent',$parent_category["id"])->orderBy('display_no','asc')->get();
+            foreach ($child_categories as $child_category) {
+                $all_categories_items[$parent_category["slug"]]["children"][$child_category["slug"]]["display_name"] = $child_category["name"];
+            }
+        }
         return $all_categories_items;
     }
-
 
 }
