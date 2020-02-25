1fe38eaa27e93da6b4d880da8c0f0c05f4406e50
keitaogawa-teamlab <in-ogawakeita@team-lab.com>
2020-02-25 17:20:47 +0900

    category all_get

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,10 +8,18 @@ class Category extends DaisyModelBase
 {
     protected $table = 'categories';
 
-    public static function getCategoryBySlug($slug): ?Category
+    public static function getCategoriesBySlug($slug): ?Category
     {
         $slug_item = Category::where('slug', $slug)->first();
         $category_items = Category::where('parent',$slug_item["id"])->orderBy('display_no','asc')->get();
         return $category_items;
     }
+
+    public static function getAllCategories(): ?Category
+    {
+        $all_categories_items = Category::all();
+        return $all_categories_items;
+    }
+
+
 }
