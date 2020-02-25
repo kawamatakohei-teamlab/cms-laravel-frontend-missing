cb4d5c9eb197a0a7688f135bf7fb9c79046b92ba
toukoubun <tanghaowen@team-lab.com>
2020-02-25 18:57:08 +0900

    footer完成

--- resources/views/general_index.blade.php
+++ resources/views/general_index.blade.php
@@ -338,11 +338,48 @@ if(Request::url() ==  '/'){
 
 </main>
 </body>
-<footer>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min.js?date=3600"></script>
-    <script src="/assets/scripts/main.js?date=3600"></script>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PS4768"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<footer class="c-footer">
+    <div class="c-footer__in">
+        <div class="c-footer__upper">
+            <div class="c-footer__logo"><a class="c-footer__logo-link" href="/"><img class="c-footer__logo-image" src="/assets/materials/logo_footer.png" alt="日本調剤"/></a>
+                <div class="c-footer__logo-text">お客さま向け情報</div>
+            </div>
+            <div class="c-footer__nav-container">
+                <nav class="c-footer-nav">
+                    <ul class="c-footer-nav__list">
+                        <li class="c-footer-nav__list-item"><a class="c-footer-nav__link" href="/corporate">企業情報トップ</a></li>
+                        <li class="c-footer-nav__list-item"><a class="c-footer-nav__link" href="/corporate/recruitment">採用情報</a></li>
+                        <li class="c-footer-nav__list-item"><a class="c-footer-nav__link" href="/corporate/ir">IR情報</a></li>
+                        <li class="c-footer-nav__list-item"><a class="c-footer-nav__link" href="/corporate/ekoukoku">電子公告</a></li>
+                        <li class="c-footer-nav__list-item"><a class="c-footer-nav__link" href="/inquiry#inquiry">お問い合わせ</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="c-footer__lower">
+            <div class="c-footer-utility">
+                <ul class="c-footer-utility__list">
+                    <li class="c-footer-utility__list-item"><a class="c-footer-utility__link" href="/security">情報セキュリティポリシー</a></li>
+                    <li class="c-footer-utility__list-item"><a class="c-footer-utility__link" href="/privacy">個人情報保護方針</a></li>
+                </ul>
+                <ul class="c-footer-utility__list">
+                    <li class="c-footer-utility__list-item"><a class="c-footer-utility__link" href="/socialpolicy">ソーシャルメディアポリシー</a></li>
+                    <li class="c-footer-utility__list-item"><a class="c-footer-utility__link" href="/actionplan">行動計画</a></li>
+                    <li class="c-footer-utility__list-item"><a class="c-footer-utility__link" href="/terms">利用規約</a></li>
+                </ul>
+                <ul class="c-footer-utility__list">
+                    <li class="c-footer-utility__list-item"><a class="c-footer-utility__link" href="/sitemap">サイトマップ</a></li>
+                </ul>
+            </div>
+            <div class="c-footer__copyright">Copyright &copy; NIHON CHOUZAI Co., Ltd. All rights reserved.</div>
+        </div>
+    </div>
 </footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min.js?date=3600"></script>
+<script src="/assets/scripts/main.js?date=3600"></script>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PS4768"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
 </html>
