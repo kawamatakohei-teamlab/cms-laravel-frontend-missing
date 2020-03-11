407695ce85e9c61dee485c5271f606aa7e966b9d
kagawayusuke <kagawayusuke@team-lab.com>
2020-03-11 14:56:18 +0900

    Revert "update: CmsCoreを使用"
    This reverts commit cd094a8d43eb5aa00bc40e9db175fea8f4ee24db.

--- app/Http/Controllers/Articles/TeacherController.php
+++ app/Http/Controllers/Articles/TeacherController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Articles;
 
 use App\Http\Controllers\ArticleController;
-use App\CmsCore\Models\Article;
-use App\CmsCore\Models\Category;
+use App\Models\Article;
+use App\Models\Category;
 
 class TeacherController extends ArticleController
 {
