e607cea142ed816fca86c090e13e4df2d032e32d
kagawayusuke <kagawayusuke@team-lab.com>
2020-03-17 15:01:45 +0900

    fix: データが無い場合を想定し、条件分岐を追加

--- resources/views/pages/articles/department/index.blade.php
+++ resources/views/pages/articles/department/index.blade.php
@@ -3,19 +3,21 @@
 @section('main')
 <main class="layout-base__main-center">
     <div class="layout-base__inner">
-        <h2 class="heading-large-white heading-large-white--low js-scroll">
-            <div class="heading-large-white__wrap">
-                <div class="heading-large-white__text">{{ $departmentListArticle->title }}</div>
-            </div>
-        </h2>
-        <h2 class="heading-large heading-large--low js-scroll">
-            <div class="heading-large__wrap">
-                <?php
-                    $contents = json_decode($departmentListArticle->contents);
-                ?>
-                <div class="heading-large__text">{{ $contents->headline }}</div>
-            </div>
-        </h2>
+        @if($departmentListArticle)
+            <h2 class="heading-large-white heading-large-white--low js-scroll">
+                <div class="heading-large-white__wrap">
+                    <div class="heading-large-white__text">{{ $departmentListArticle->title }}</div>
+                </div>
+            </h2>
+            <h2 class="heading-large heading-large--low js-scroll">
+                <div class="heading-large__wrap">
+                    <?php
+                        $contents = json_decode($departmentListArticle->contents);
+                    ?>
+                    <div class="heading-large__text">{{ $contents->headline }}</div>
+                </div>
+            </h2>
+        @endif
         <div class="js-scroll animation-slide-in-bottom">
             <div class="thumbnail-list thumbnail-list--cols3">
                 <ul class="thumbnail-list__list">
@@ -42,32 +44,34 @@
                 </ul>
             </div>
         </div>
-        <div class="js-scroll animation-slide-in-bottom">
-            <div class="multi-banner">
-                <div class="multi-banner__inner">
-                    <?php
-                        $contents = json_decode($introductionRelatedPageArticle->contents);
-                    ?>
-                    <div class="multi-banner__detail">
-                        <div class="multi-banner__title">{!! $introductionRelatedPageArticle->title !!}</div>
-                        <div class="multi-banner__text">{{ $contents->body }}</div>
-                        <div class="multi-banner__button">
-                            <a class="button-base button-base--yellow" href="{{ $contents->button_url }}">{{ $contents->button_word }}</a>
-                        </div>
-                    </div>
-                    <div class="multi-banner__visual">
-                        <div class="multi-banner__image">
-                            <div class="js-scroll animation-image-ratio">
-                                <img class="animation-image-ratio__img" src="{{ $contents->right_image }}" alt="">
+        @if($introductionRelatedPageArticle)
+            <div class="js-scroll animation-slide-in-bottom">
+                <div class="multi-banner">
+                    <div class="multi-banner__inner">
+                        <?php
+                            $contents = json_decode($introductionRelatedPageArticle->contents);
+                        ?>
+                        <div class="multi-banner__detail">
+                            <div class="multi-banner__title">{!! $introductionRelatedPageArticle->title !!}</div>
+                            <div class="multi-banner__text">{{ $contents->body }}</div>
+                            <div class="multi-banner__button">
+                                <a class="button-base button-base--yellow" href="{{ $contents->button_url }}">{{ $contents->button_word }}</a>
                             </div>
                         </div>
-                    </div>
-                    <div class="multi-banner__sp-button">
-                        <a class="button-base button-base--yellow" href="">{{ $contents->button_word }}</a>
+                        <div class="multi-banner__visual">
+                            <div class="multi-banner__image">
+                                <div class="js-scroll animation-image-ratio">
+                                    <img class="animation-image-ratio__img" src="{{ $contents->right_image }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="multi-banner__sp-button">
+                            <a class="button-base button-base--yellow" href="">{{ $contents->button_word }}</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endif
     </div>
 </main>
 @endsection
