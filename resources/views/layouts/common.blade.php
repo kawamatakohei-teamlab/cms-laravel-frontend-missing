fee1682d4dee26b216e5f4a8650a45c572737c7e
kawamatakohei-teamlab <kawamatakohei@team-lab.com>
2020-03-23 12:22:43 +0900

    update: parts/ から partialsへ移動し、構成を一部見直し

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
-    @include('parts.head')
-    @include('parts.head_javascript')
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="format-detection" content="telephone=no, address=no, email=no">
+    <meta name="robots" content="follow,index">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>title</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta property="og:title" content="大阪芸術大学">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="siteURL/">
+    <meta property="og:locale" content="ja_JP">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="http://www.osaka-geidai.ac.jp/assets/images/ogp.jpg">
+    <link rel="icon" sizes="196x196" href="/assets/images/favicon-196x196.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/images/apple-touch-icon-152x152.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.ico">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="msapplication-TileImage" content="/assets/images/mstile-144x144.png">
+    <meta name="msapplication-TileColor" content="#fff">
+    <meta name="theme-color" content="#fff">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Hind:400,500,600,700">
+    <link rel="stylesheet" href="/styles/touch-device-style.css">
+
+    @include('partials.layout.head')
+    @include('partials.layout.head_javascript')
   </head>
   <body class="base-page">
     @yield('header_javascript')
-    @include('parts.header')
+    @include('partials.layout.header')
 
     @yield('breadcrumb')
 
@@ -14,8 +39,8 @@
       @yield('main')
     </div>
 
-    @include('parts.footer')
-    @include('parts.modal')
-    @include('parts.footer_javascript')
+    @include('partials.layout.footer')
+    @include('partials.layout.modal')
+    @include('partials.layout.footer_javascript')
     </body>
 </html>
